dd418fd014fbb0a4bbf47d80128585d3c002f1f5
leofab <leofabr3@gmail.com>
2022-07-14 16:52:21 -0300

    try catch implement

---
+++ script.js
@@ -0,0 +1,17 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest';
+
+const getCats = async () => {
+    try {
+        const data = await fetch(BASE_URL);
+        const json = await data.json();
+    
+        return json.wepurl;
+
+    }
+
+
+
+    catch(e) {
+    console.log(e.message);
+    }
+}
